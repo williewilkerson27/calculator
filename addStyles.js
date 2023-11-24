a51f22d725069fef6a0a2ff71e70ca903ed80c6b
williewilkerson27 <williamwilkerson27@outlook.com>
2023-11-24 18:10:40 -0500

    finally got the calc buttons to show

--- addStyles.js
+++ addStyles.js
@@ -18,7 +18,7 @@ const currentNumClass = 'flex max-w-lg h-2/3 pb-2 text-xl';
 const currentNum = document.querySelector('.currentNum');
 currentNumClass.split(' ').forEach((cls) => currentNum.classList.add(cls));
 
-const buttons = document.querySelectorAll("button");
+const buttons = document.querySelectorAll('button');
 buttons.forEach((btn) => {
     addClasses(btn);
 });
@@ -29,19 +29,20 @@ function addClasses(button) {
     const btnOpClass = 'hover:bg-blue-500 border-blue-500';
     const btnEqClass = "hover:bg-red-500 border-red-500";
     const btnDecClass = "hover:bg-yellow-500 border-yellow-500";
-}
 
-if (button.classList.value === 'number') {
-    btnNumClass.split(' ').forEach((cls) => button.classList.add(cls));
-} else if (button.classList.value === 'operator') {
-    btnOpClass.split(' ').forEach((cls) => button.classList.add(cls))
-} else if (button.classList.value === 'clear') {
-    btnClearClass.split(' ').forEach((cls) => button.classList.add(cls))
-} else if (button.classList.value === "equal") {
-    btnEqClass.split(" ").forEach((cls) => button.classList.add(cls));
-} else if (button.classList.value === "decimal") {
-    btnDecClass.split(" ").forEach((cls) => button.classList.add(cls));
-}
+    if (button.classList.value === 'number') {
+        btnNumClass.split(' ').forEach((cls) => button.classList.add(cls));
+    } else if (button.classList.value === 'operator') {
+        btnOpClass.split(' ').forEach((cls) => button.classList.add(cls))
+    } else if (button.classList.value === 'clear') {
+        btnClearClass.split(' ').forEach((cls) => button.classList.add(cls))
+    } else if (button.classList.value === "equal") {
+        btnEqClass.split(" ").forEach((cls) => button.classList.add(cls));
+    } else if (button.classList.value === "decimal") {
+        btnDecClass.split(" ").forEach((cls) => button.classList.add(cls));
+    }
 
-const baseClass = 'number flex w-12 h-12 justify-center items-center m-1 font-bold py-2 px-2 border hover:border-transparent rounded text-white border-2 text-2xl';
-baseClass.split(' ').forEach((cls) => button.classList.add(cls))
+const baseClass = 
+'number flex w-12 h-12 justify-center items-center m-1 font-bold py-2 px-2 border hover:border-transparent rounded text-white border-2 text-2xl';
+baseClass.split(' ').forEach((cls) => button.classList.add(cls));
+}
